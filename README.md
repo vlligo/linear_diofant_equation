068440f03ee5c027bde56f0d65bd487f76a27b1d
Vladimir <vladimir2balakin@yandex.ru>
2022-09-07 21:50:18 +0300

    Added README.md and solver

---
+++ README.md
@@ -0,0 +1,14 @@
+# Diophantine linear equation solver
+
+## What is this project about?
+This program can solve [Diophantine linear equations](https://brilliant.org/wiki/linear-diophantine-equations-one-equation/).
+$$\alpha_1 \cdot x_1 + \alpha_2 \cdot x_2 + \alpha_3 \cdot x_3 + ... + \alpha_n \cdot x_n = c$$
+It also prints the linear combination of gcd of the coefficients you entered.
+
+## Basic usage
+Run ```main.py```. You'll need to enter coefficients you want to use divided by spaces.
+Then you'll need to enter what the equation will be equal to. And finally confirm you equation.
+In the next line type `Y` if you want zero therms(with zero coefficients) to be shown and `n` if you don't.
+
+The program will print gcd of the coefficients you entered, the linear combination of them. 
+Finally, all solution of the equation will be shown.
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import sys
 class Matrix:
     def __init__(self, a):
         self.n = len(a)
-        self.table = [[0 for j in range(n)] for i in range(n + 1)]
+        self.table = [[0 for _ in range(n)] for _ in range(n + 1)]
         for i in range(1, n + 1):
             self.table[i][i - 1] = 1
         self.table[0] = a
@@ -24,29 +24,25 @@ class Matrix:
         if first >= self.n or second >= self.n:
             return
         a = self.table[0][first]
-        b = self.table[0][second]
+        bb = self.table[0][second]
         if a == 0:
             return
-        if b == 0:
+        if bb == 0:
             return
-        ans = [0 for i in range(n + 1)]
-        ans[0] = a % b
-        k = a // b
+        ans = [0 for _ in range(n + 1)]
+        ans[0] = a % bb
+        k = a // bb
         self.add(first, second, -k)
-
-    def swap(self, first, second):
-        for i in range(self.n + 1):
-            self.table[i][first], self.table[i][second] = self.table[i][second], self.table[i][first]
 
     def gcd(self, first, second, f=True):
         if self.table[0][first] == 0:
             if f:
-                self.swap(first, second)
+                self.add(first, second)
             return
         self.mod(second, first)
         self.gcd(second, first, False)
         if f and self.table[0][first] == 0:
-            self.swap(first, second)
+            self.add(first, second)
 
     def println(self):
         for i in self.table:
@@ -56,19 +52,45 @@ class Matrix:
 
 
 n = 1
+c = 0
 m = Matrix([])
 b = []
 printing_zero = 0
 
 
 def read():
-    global n, m, b, printing_zero
-    n = int(input())
-    a = list(map(int, input().split()))
-    printing_zero = input("Print terms with zero coefficient?[Y/n]\n").lower()
+    global n, m, b, printing_zero, c
+    reentering = True
+    aa = []
+    while reentering:
+        a = list(map(int, input("Enter coefficients you want to use: ").split()))
+        n = len(a)
+        c = int(input("What will on the right side of the equation?(It must be a number)\n"))
+        print("The equation you entered is: ", end='')
+        first_sign = False
+        for i in range(n):
+            if first_sign:
+                if a[i] >= 0:
+                    print(" + ", end='')
+                else:
+                    print(" - ", end='')
+            else:
+                first_sign = True
+                if a[i] < 0:
+                    print("-", end='')
+            if abs(a[i]) != 1:
+                print("{}*x_{}".format(abs(a[i]), i + 1), end='')
+            else:
+                print("x_{}".format(i + 1), end='')
+        print("?[Y/n]")
+        ans = input().lower()
+        reentering = ans != 'y'
+        if not reentering:
+            aa = a.copy()
+    printing_zero = input("Print terms with zero coefficient in the linear combination?[Y/n]\n").lower()
     printing_zero = bool(printing_zero == 'y')
-    b = a.copy()
-    m = Matrix(a)
+    b = aa.copy()
+    m = Matrix(aa)
 
 
 def solve():
@@ -77,7 +99,8 @@ def solve():
 
 
 def print_answer():
-    print("{} = ".format(m.table[0][0]), end='')
+    print("GCD of all coefficients is {}.".format(m.table[0][0]))
+    print("Linear combination: {} = ".format(m.table[0][0]), end='')
     print("{}*{}".format(m.table[1][0], b[0]), end='')
     for i in range(1, n):
         if m.table[i + 1][0] == 0 and not printing_zero:
@@ -87,19 +110,34 @@ def print_answer():
         else:
             print(" - ", end='')
         print("{}*{}".format(abs(m.table[i + 1][0]), b[i]), end='')
-    for j in range(1, n):
-        if m.table[1][j] >= 0:
-            print(" + ")
-        else:
-            print(" - ")
-        for i in range(1, n + 1):
-            if m.table[i][j] == 0 and not printing_zero:
-                continue
-            if m.table[i][j] >= 0:
-                print(" + ", end='')
-            else:
-                print(" - ", end='')
-            print("{}*t_{}".format(abs(m.table[i][j]), j), end='')
+    print()
+    if c % m.table[0][0] != 0:
+        print("This equation has no solutions")
+    else:
+        coefficient = c // m.table[0][0]
+        for j in range(1, n + 1):
+            print("x_{} = ".format(j), end='')
+            first_sign = False
+            if not(not printing_zero and m.table[j][0] == 0):
+                print(m.table[j][0] * coefficient, end='')
+                first_sign = True
+            for i in range(1, n):
+                if m.table[j][i] == 0 and not printing_zero:
+                    continue
+                if printing_zero or first_sign:
+                    if m.table[j][i] >= 0:
+                        print(" + ", end='')
+                    else:
+                        print(" - ", end='')
+                else:
+                    first_sign = True
+                    if m.table[j][i] < 0:
+                        print("-", end='')
+                if abs(m.table[j][i]) * coefficient != 1:
+                    print("{}*t_{}".format(abs(m.table[j][i]) * coefficient, i), end='')
+                else:
+                    print("t_{}".format(abs(i)), end='')
+            print()
 
 
 if __name__ == '__main__':
